c9bb336374932d516a0a688be2ba5eb1aa954002
ZacharyPainter <zacharypainter@knights.ucf.edu>
2019-04-04 00:19:49 +0000

    Test Scripts Updated

--- testScripts/startAlice.js
+++
@@ -1,9 +0,0 @@
-// Start Alice and warm up wiht some ether transactions
-//
-//miner.start()
-eth.defaultAccount=alice
-personal.unlockAccount(alice, "foobar123", 10000);
-eth.sendTransaction({from:alice, to:lily, value: web3.toWei(0.020, "ether")})
-eth.sendTransaction({from:alice, to:lily, value: web3.toWei(0.019, "ether")})
-eth.sendTransaction({from:alice, to:bob, value: web3.toWei(0.018, "ether")})
-eth.sendTransaction({from:alice, to:bob, value: web3.toWei(0.017, "ether")})
